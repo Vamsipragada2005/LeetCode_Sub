569ac7cf2cef28c225173880674fc789e561a536
PRAGADA VAMSI <154915845+Vamsipragada2005@users.noreply.github.com>
2026-06-01 15:58:53 +0530

    Time: 5 ms (96.09%), Space: 44.5 MB (51.02%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int n = cost.length;
+        int total = 0;
+        for(int i=n-1;i>=0;i--){
+            if((n-i-1)%3!=2){
+                total+=cost[i];
+            }
+        }
+        return total;
+
+        
+    }
+}
